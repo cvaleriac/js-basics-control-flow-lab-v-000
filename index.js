e77b5cd2a023b0c5020dc5db1df92397984a8351
valeria campilongo <cvaleriac@hotmail.it>
2019-05-14 14:12:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function scuberGreetingForFeet(feet){
   if (feet <= 400) {
-  return 'This one is on me!';
-} else if (feet >= 2000){
-  return 'I will gladly take your thirty bucks.';
-}
-  else (feet >2500) {
-    return 'No can do.';
+    return 'This one is on me!';
+  } else if (199 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
   }
-
 }
