c78c09ff7fe6b764ec62a1d81232a1638699776c
valeria campilongo <cvaleriac@hotmail.it>
2019-05-14 14:28:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function switchOnCharmFromTip(tip){
   case 'generous':
         return 'Thank you so much.';
         break;
-    case 'not generous':
+    case 'not as generous':
         return = 'Thank you.';
         break;
     default :
